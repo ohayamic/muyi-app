78476b2d99bba5c4df66e8a59e3f4f0f77cd2ebe
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2023-02-26 23:11:54 +0100

    Added routes to application

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MuyiMainComponent from "./MuyiMainComponent";
+import MuyiEditForm from "./MuyiForm/MuyiEditForm";
 
+const router = createBrowserRouter([
+  { path: "/", element: <MuyiMainComponent /> },
+  { path: "edituser/:firstname", element: <MuyiEditForm /> },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MuyiMainComponent />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
